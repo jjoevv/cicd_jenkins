86fa5c5413404222f649d589151eb2351d332bfb
Ha Nguyen Thao Vy <hhnnttvy@gmail.com>
2025-07-14 09:51:52 +0700

    fix groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,12 +1,28 @@
-def call(String imageName, String dir, String tag, String DOCKERHUB_USERNAME, String DOCKERHUB_PASSWORD) {
-    echo "🚀 Pushing Docker images to Docker Hub..."
 
-    sh """
-    echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+def call(String imageName, String tag, String DOCKERHUB_USERNAME, String DOCKERHUB_PASSWORD) {
+    stage("Push Docker Image for ${service}") {
+        def imageName = service == 'frontend' ? "${env.IMAGE_FE}" : "${env.IMAGE_BE}"
+        def buildTag = "${imageName}:${env.TAG}"
+        def latestTag = "${imageName}:latest"
 
-    docker push $imageName:latest
-    docker push $imageName:$tag
+        if (params.SKIP_PUSH_IMAGE) {
+            echo "Skipping Docker push for ${service} because SKIP_PUSH_IMAGE is true."
+            return
+        }
 
-    docker logout
-    """
+        echo "Logging in to Docker Hub..."
+        
+    
+        sh """
+        echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+        echo "🚀 Pushing Docker images to Docker Hub..."
+
+        docker push $imageName:latest
+        docker push $imageName:$tag
+
+        docker logout
+        """
+    }
+
+    
 }
