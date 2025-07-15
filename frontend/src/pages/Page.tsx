a45676dde240895ddccb2db85f23d746e5bbbe7b
Ha Nguyen Thao Vy <112704960+jjoevv@users.noreply.github.com>
2025-07-15 16:58:30 +0700

    Revert "Merge fe"

--- frontend/src/pages/Page.tsx
+++
@@ -1,8 +0,0 @@
-// app/page.tsx
-export default function Home() {
-  return (
-    <main>
-      <h1>My Simple Blog</h1>
-    </main>
-  );
-}
--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,21 +1,9 @@
+def call (String imageName, String dir, String tag) {
+    sh 'docker info || { echo "Docker is not running. Exiting."; exit 1; }'
 
-def call (String imageName, String buildTag) {
-    stage("Build Docker Image for ${service}") {
-        def imageName = service == 'frontend' ? "${env.IMAGE_FE}" : "${env.IMAGE_BE}"
-        def buildTag = "${imageName}:${env.TAG}"
-        def latestTag = "${imageName}:latest"
+    echo "🛠 Building Docker images for ${imageName}..."
 
-        if (params.SKIP_BUILD_IMAGE) {
-            echo "Skipping Docker build for ${service} because SKIP_BUILD_IMAGE is true."
-            return
-        }
-        
-        dir(service) {
-            sh 'docker info || { echo "Docker is not running. Exiting."; exit 1; }'
-            echo "Building Docker image for ${service}..."
-            sh """
-                docker build -t $imageName:latest -t $imageName:$buildTag
-            """
-        }
-    }
+    sh """
+    docker build -t $imageName:latest -t $imageName:$TAG $dir
+    """
 }
