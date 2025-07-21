f30da0f68782000883b40830b0e4663898a6c7ed
Ha Nguyen Thao Vy <hhnnttvy@gmail.com>
2025-07-21 14:14:04 +0700

    test

--- frontend/tests/Home.test.js
+++ frontend/tests/Home.test.js
@@ -2,7 +2,7 @@ import { describe, it, expect } from "vitest";
 import { add } from "../src/functions_tests/math";
 
 describe("add function", () => {
-  it("should return 3 when adding 1 and 2", () => {
+  it("Should return 3 when adding 1 and 2 in this test", () => {
     expect(add(1, 2)).toBe(3);
   });
 });
